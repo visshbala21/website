43e3f786d70a9b5fecb49f43a839f613dad3ad16
visshbala21 <visshbala12@gmail.com>
2024-07-21 23:39:52 -0500

    js for fun

--- auth.js
+++ auth.js
@@ -1,11 +1,11 @@
 function checkPassword() {
     const correctPassword = "4alltheD@wgZ"; 
-    const userPassword = prompt("Please enter the password to access this page:");
+    const userPassword = prompt("Please enter the password to see my stats, awards, resume, and more!: ");
 
     if (userPassword === correctPassword) {
         document.getElementById("protected-content").style.display = "block";
     } else {
-        alert("Nah that ain't gang nt. You will be redirected to the homepage.");
+        alert("Nah that ain't gang nt. Back the lobby bro.");
         window.location.href = "index.html"; 
     }
 }
