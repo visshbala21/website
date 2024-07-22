43df262c8cc09c53febf329e6aaa5e5e1d64a1bc
visshbala21 <visshbala12@gmail.com>
2024-07-22 09:52:49 -0500

    .

--- auth.js
+++ auth.js
@@ -1,13 +1,11 @@
-function checkPassword(){
-    const correctPassword = "4alltheD@wgZ"
-    const userPassword = prompt("Please enter the password to access thsi page:");
+function checkPassword() {
+    const correctPassword = "4alltheD@wgZ"; 
+    const userPassword = prompt("Please enter the password to see my stats, awards, resume, and more!: ");
 
-
-    if (userPassword == correctPassword){
-        document.getElementById("protected-content").style.display = "block"
-    }
-    else{
-        alert("Nah that ain't it gang nt. Back to the lobby bro");
-        window.location.href = "index.html";
+    if (userPassword === correctPassword) {
+        document.getElementById("protected-content").style.display = "block";
+    } else {
+        alert("Nah that ain't gang nt. Back the lobby bro.");
+        window.location.href = "index.html"; 
     }
 }
